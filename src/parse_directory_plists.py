936fee97682fb269be4fce2fd87d74cfb11462be
Dad <twisterfish.ai@gmail.com>
2024-11-28 09:57:38 -0600

    cleanup and comments

--- src/parse_directory_plists.py
+++ src/parse_directory_plists.py
@@ -1,11 +1,27 @@
 #######################################################################
-# Python script to parse an iTunes XML file and output the data to a CSV file
+# Python script that will search through the specified directory and
+# parse all iTunes PList files and generate CSV or SQL files from the XML
+# data. The script will read the XML data and extract the track name, artist
+# name, album name, date added, track length, number of plays, and beats per
+# minute. The script will then write the data to a CSV or SQL file based on
+# the flags set in the script. The script will also print the data to the
+# console if the printout flag is set to True.  
+# This script is designed to be run from the command line and will require 
+# the user to modify the path to the iTunes XML file, the output CSV and SQL files,
+# and the flags to control the output of the script. The script will also
+# generate a timestamp to append to the output file names to ensure that
+# the files are unique. The script will use the file name to extract the date
+# that the playlist was created and then reformat the date to be in the format
+# YYYY-MM-DD. 
 # Instructions:
 # 1. Open the script in your favorite text editor
-# 2. Change the path to the iTunes XML file on line 34
-# 3. Change the path to the output CSV and SQL files on lines 37 and 40
-# 4. Save the script changes
-# 5. Run the script by typing 'python3 parseplist.py' in the terminal
+# 2. Set the flags to true or false to control the output of the script
+# 3. Change the path to the directory where the iTunes XML files are stored
+#    in the variable 'search_path' on the paths section of the script
+# 4. Change the path to the output CSV and/or SQL files you wish to create
+#    in the variables 'target_csv' and/or 'target_sql' in the paths section of the script
+# 5. Save the script changes
+# 6. Run the script by typing 'python3 parse_directory_plists.py' in the terminal
 #######################################################################
 
 import plistlib
@@ -16,68 +32,71 @@ from pathlib import Path
 
 ####################################################################### 
 # These are global variables used to control the output of the script
-# Set the flags to True to enable the output for that type
 #######################################################################
 
+# Flags to control the output of the script - set to True to enable the output
 write_csv = False # Set to True to write to a CSV file
 write_sql = True # Set to True to write out a SQL import file
 printout = False # Set to True to print to the console
 
-count = 0 # This is a counter for the number of tracks processed
+# Variables used to store data for the CSV and SQL files
+csvLine = "" # This is the line buffer that will be written to the CSV file
+sqlLine = "" # This is the line buffer that will be written to the SQL file
+csv_file_handle = None # This is the CSV file that will be written to if used
+sql_file_handle = None # This is the SQL file that will be written to if used
+plist_file_path = Path('') # This is the PLIST file that will be read after binding to the search path
 
+# Informational variables
 today = datetime.datetime.now() # This is the current date and time
-today = today.strftime("%m-%d-%YT%H-%M-%S") # This is the current date and time as a string
+today = today.strftime("%m-%d-%YT%H-%M-%SZ") # This is the current date and time as a string
+count = 0 # This is a counter for the number of tracks processed
 
 #######################################################################
 # These are the paths to the files that will be used in the script
 #######################################################################
 
-# This is a switch to find all xml files in the specified directory
+# This is the directory that will be searched for the PLIST files
+# Add the directory path here
 search_path = Path('../data/OneDrive-2024-11-27')
 
-# This is the PLIST file that will be read from - add the file path here
-plist_file_path = Path('../data/Equinox-09-15-24.xml')
-
-# This is the CSV file that will be written to - add the file path here
+# This is the CSV file that will be written to - 
+# Add the file path and name here
 target_csv = Path('../data/' + 'Playlist_Export.' + today + '.csv')
 
-# This is the SQL file that will be written to - add the file path here
+# This is the SQL file that will be written to - 
+# Add the file path and name here
 target_sql = Path('../data/' + 'Playlist_Export.' + today + '.sql')
 
 #######################################################################
 # This is the main part of the script that reads the plist file
 #######################################################################
 
-# initialize the variables
-csvLine = "" # This is the line that will be written to the CSV file
-sqlLine = "" # This is the line that will be written to the SQL file
-csv_file_handle = None # This is the CSV file that will be written to if used
-sql_file_handle = None # This is the SQL file that will be written to if used
+ # If the flag is set to write to a CSV file, write the header line first
+if write_csv:
+    csv_file_handle = open( target_csv, 'a' )
+    csvLine = "date_played,track_name,artist_name,album_name,date_added,track_length,num_plays,beats_per_minute\r\n" # This is the header line for the CSV file
+    csv_file_handle.write(csvLine)
+    csvLine = "" # Reset the line variable
+
+# If the flag is set to write to a SQL file, write the 'insert' clause line first
+if write_sql:
+    sql_file_handle = open( target_sql, 'a' )
+    sqlLine = "INSERT INTO spin_playlists (date_played,track_name, artist_name, album_name, date_added, track_length, num_plays, beats_per_minute) VALUES \r\n" # This is the header line for the SQL file
+    sql_file_handle.write(sqlLine)
+    sqlLine = "" # Reset the line variable
+
 
 for file in os.listdir(search_path):
     if file.endswith(".xml"):
-
+        # Print the file name to the console
         print(os.path.join(search_path, file))
+        # Set the path to the plist file
         plist_file_path = Path(os.path.join(search_path, file))
 
         # Parse the date from the file name and then reformat it to be in the format YYYY-MM-DD
         parsedDate = re.search("[0-9][0-9]-[0-9][0-9]-[0-9][0-9]", plist_file_path.stem).group(0)
         correctformat = parsedDate.split("-")
         parsedDate = "20" + correctformat[2] + "-" + correctformat[0] + "-" + correctformat[1]
-
-        # If the flag is set to write to a CSV file, write the header line first
-        if write_csv:
-            csv_file_handle = open( target_csv, 'a' )
-            csvLine = "date_played,track_name,artist_name,album_name,date_added,track_length,num_plays,beats_per_minute\r\n" # This is the header line for the CSV file
-            csv_file_handle.write(csvLine)
-            csvLine = "" # Reset the line variable
-
-        # If the flag is set to write to a SQL file, write the header line first
-        if write_sql:
-            sql_file_handle = open( target_sql, 'a' )
-            sqlLine = "INSERT INTO spin_playlists (date_played,track_name, artist_name, album_name, date_added, track_length, num_plays, beats_per_minute) VALUES \r\n" # This is the header line for the SQL file
-            sql_file_handle.write(sqlLine)
-            sqlLine = "" # Reset the line variable
 
         # Open the plist file and load the data
         with open(plist_file_path, 'rb') as infile:
@@ -212,8 +231,11 @@ for file in os.listdir(search_path):
 
                 csvLine = ""
                 sqlLine = ""
+
+        # Close the plist file so we don't leak file handles
+        infile.close() 
     else:
-        print("No files found")
+        print("No plist files found")
         print(search_path)
             
 if write_csv:
