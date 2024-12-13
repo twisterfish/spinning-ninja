a37b7968c63c31d1a8efec2af21b53eb6bd55fbc
Dad <twisterfish.ai@gmail.com>
2024-12-13 12:40:14 -0600

    table formatting

--- src/wp_snippets.php
+++ src/wp_snippets.php
@@ -2,12 +2,11 @@
 global $wpdb;
 $wpdb->show_errors();
 
-
 echo "<h1>Malcolm's Tracks</h1>";
 
 echo " <table style=\"font-family: arial, sans-serif; border-collapse: collapse; width: 100%;\">";
 echo "  <thead>
-            <tr style=\"border: 1px solid #dddddd; text-align: left; padding: 8px;background-color: #b0acac;\">
+            <tr style=\"border: 1px solid #dddddd; text-align: left; padding: 8px;background-color: #f2f3fc;\">
                 <th style=\"border: 1px solid #dddddd; text-align: left; padding: 8px;\">Date Played</th>
                 <th style=\"border: 1px solid #dddddd; text-align: left; padding: 8px;\">Track Name</th>
                 <th style=\"border: 1px solid #dddddd; text-align: left; padding: 8px;\">Artist Name</th>
@@ -20,8 +19,18 @@ echo "  <thead>
         <tbody>";
 
 $tracks = $wpdb->get_results("SELECT * FROM spin_playlists ORDER BY date_played DESC");
-foreach ($tracks as $track) {   
-    echo "<tr :nth-child(even)background-color: #b0acac;>
+$oddEven = true;
+foreach ($tracks as $track) {
+
+    if ($oddEven) {
+        echo "<tr style=\"background-color: #f2f2f2;\">";
+        $oddEven = false;
+    } else {
+        echo "<tr style=\"background-color: #c9bdbd;\">";
+        $oddEven = true;
+    }
+
+    echo "
             <td style=\"border: 1px solid #dddddd; text-align: left; padding: 8px;\">" . date("F j, Y", strtotime($track->date_played)) . "</td>
             <td style=\"border: 1px solid #dddddd; text-align: left; padding: 8px;\">" . $track->track_name . "</td>
             <td style=\"border: 1px solid #dddddd; text-align: left; padding: 8px;\">" . $track->artist_name . "</td>
